c6ce2b1d21c06cd04a993db411b0c69f01d4c05d
Saikiran <Saikiran@gmail.com>
2026-08-19 19:59:55 +0530

    deploy final last file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,40 @@
 terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = ">= 3.10"
-      }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.10"
     }
-
+  }
 }
+
 provider "azurerm" {
-    features {}
-  
+  features {}
 }
-
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.location
 }
 
 resource "azurerm_service_plan" "app_service" {
-  name = var.app_service_plan_name
-  resource_group_name = var.resource_group_name
-  location = var.location
-  os_type = "Windows"
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  os_type  = "Windows"
   sku_name = "B1"
 }
 
 resource "azurerm_windows_web_app" "web_app" {
-  name = var.web_app_name
-  resource_group_name = var.resource_group_name
-  location = var.location
-  service_plan_id = azurerm_service_plan.app_service.id
+  name                = var.web_app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.app_service.id
 
   site_config {
     application_stack {
       dotnet_version = "v10.0"
-      current_stack = "dotnet"
+      current_stack  = "dotnet"
     }
   }
 }
