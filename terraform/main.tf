18316a9d84c6348b336ad000269b390851ea23ef
Saikiran <Saikiran@gmail.com>
2026-08-19 19:19:55 +0530

    tf commit

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">= 3.10"
+      }
+    }
+
+}
+provider "azurerm" {
+    features {}
+  
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "app_service" {
+  name = var.app_service_plan_name
+  resource_group_name = var.resource_group_name
+  location = var.location
+  os_type = "Windows"
+  sku_name = "B1"
+}
+
+resource "azurerm_windows_web_app" "web_app" {
+  name = var.web_app_name
+  resource_group_name = var.resource_group_name
+  location = var.location
+  service_plan_id = azurerm_service_plan.app_service.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "v10.0"
+      current_stack = "dotnet"
+    }
+  }
+}
---
+++ terraform/output.tf
@@ -0,0 +1,8 @@
+output "web_app_name" {
+    value = azurerm_windows_web_app.web_app.name
+  
+}
+
+output "webapp_default_hostname" {
+    value = azurerm_windows_web_app.web_app.default_hostname
+}
