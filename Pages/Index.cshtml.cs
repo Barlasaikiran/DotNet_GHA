973bd73e88fc87f516d5b53fe6b1d00d8ce46ef3
Saikiran <Saikiran@gmail.com>
2026-08-19 18:42:09 +0530

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HDFC_GHA.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
